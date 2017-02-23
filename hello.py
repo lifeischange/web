fe11aea32b1fd0877c16d0ab3079300cd58dbeff
lifeischange <927491949@qq.com>
2017-02-23 16:44:01 +0800

    第二天学了下jinja和bootstrap，熟悉了下mvc模型

--- hello.py
+++ hello.py
@@ -1,12 +1,21 @@
-# !/usr/bin/dev/python
+# !/usr/bin/dev python
 # coding:utf-8
 
-from flask import Flask
+from flask import Flask,render_template
+from flask_script import Manager 
+from flask_bootstrap import Bootstrap
 app=Flask(__name__)
 
+manager=Manager(app)
+bootstrap=Bootstrap(app)
 @app.route("/")
 def index():
-    return '<h1>Hello World! </h1>'
+    return render_template('index.html') 
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template('user.html',name=name)
+
 
 if __name__=="__main__":
-    app.run(debug=True)
+    manager.run()
