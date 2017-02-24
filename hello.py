1032dcb7079a70dfae5e2e3b064cfbcb4b879992
lifeischange <927491949@qq.com>
2017-02-24 14:10:07 +0800

    这是第三天，重新熟悉下表单及渲染，又理解多了点。

--- hello.py
+++ hello.py
@@ -1,12 +1,43 @@
 # !/usr/bin/dev/python
 # coding:utf-8
 
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask,render_template
+from flask_script import Manager
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 
-@app.route("/")
+app=Flask(__name__)
+manager=Manager(app)
+moment=Moment(app)
+bootstrap=Bootstrap(app)
+
+app.config['SECRET_KEY']='hard to guess' 
+
+class NameForm(Form):
+	name=StringField(u"我的名字",validators=[Required()])
+	submit=SubmitField(u"提交")
+
+@app.route("/",methods=['GET','POST'])
 def index():
-    return '<h1>Hello World! </h1>'
+	name=None
+	form=NameForm()
+	if form.validate_on_submit():
+		name=form.name.data
+		form.name.data=''
+	return render_template("base.html",form=form,current_time=datetime.utcnow())
+
+@app.route("/user/<name>")
+def user(name):
+	form=NameForm()
+	if form.validate_on_submit():
+		name=form.name.data
+		form.name.data=''
+	return render_template("base.html",name=name,form=form,current_time=datetime.utcnow())
+
 
 if __name__=="__main__":
-    app.run(debug=True)
+    manager.run()
